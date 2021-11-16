5e8e17705aa5a370c57229c741b37f4718c93941
Felix Saiz <felixsaiz97@gmail.com>
2021-11-16 13:10:06 +0000

    day_10 refactor 1

--- day_10/main.py
+++ day_10/main.py
@@ -4,15 +4,15 @@ import itertools
 def main():
     with open("input.txt") as file:
         adapters = [int(line) for line in file]
-    
+
     adapters.append(0)
     adapters.append(max(adapters) + 3)
     adapters.sort()
     pairs = itertools.pairwise(adapters)
 
-    results = {1:0, 2:0, 3:0}
+    results = {1: 0, 2: 0, 3: 0}
     for pair in pairs:
-        #print(pair)  # debug
+        # print(pair)  # debug
         results[pair[1] - pair[0]] += 1
 
     print(results)
