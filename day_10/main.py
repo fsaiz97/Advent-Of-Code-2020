3428dd411f4adbbc7e0bb2f8588ac8e0b52e401a
Felix <felixsaiz97@gmail.com>
2021-11-18 19:15:59 +0000

    Day 10 Added part 2 slow algorithm

--- day_10/main.py
+++ day_10/main.py
@@ -1,12 +1,36 @@
 import itertools
+import argparse
+
+
+parser = argparse.ArgumentParser(description="Takes the input file as an argument")
+parser.add_argument('input', help="Stores the input text file.")
+args = parser.parse_args()
+
+
+def count_paths(current, adapters, count=0):
+    for next_ in range(current+1, current+4):
+        if next_ not in adapters:
+            continue
+
+        if next_ == adapters[-1]:
+            count += 1
+            break
+        else:
+            count = count_paths(next_, adapters, count)
+
+        print(f"For {current}, {count =}")
+    return count
 
 
 def main():
-    with open("input.txt") as file:
+    with open(args.input) as file:
         adapters = [int(line) for line in file]
 
+    # adds socket and device joltages
     adapters.append(0)
     adapters.append(max(adapters) + 3)
+
+    # order adapters and collates consecutive pairs
     adapters.sort()
     pairs = itertools.pairwise(adapters)
 
@@ -16,7 +40,11 @@ def main():
         results[pair[1] - pair[0]] += 1
 
     print(results)
-    print(results[1] * results[3])
+    print("part 1 answer:", results[1] * results[3])
+
+    count = count_paths(adapters[0], adapters)
+
+    print("part 2 answer:", count)
 
 
 if __name__ == '__main__':
